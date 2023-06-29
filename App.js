e590d7cdf1c42551fd68f319a3e55c82aaab02ca
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-06-29 23:26:49 +0200

    update to the app # shiba added

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Dette er en klog og dygtig finnsk laphund. Alle elsker den.  </Text>
+      <Text>Dette er en klog og dygtig finnsk laphund. Alle elsker den.</Text>
       <View style={{ height: 20 }} />
       <Image
         style={styles.logo}
-        source={require('./assets/lapp-small.png')}
+        source={count < 20 ? require('./assets/lapp-small.png') : require('./assets/shiba.jpg')}
       />
+      <View style={{ height: 20 }} />
+      <Text>Klik på denne knap tyve gange og se hvad der sker.</Text>
+      <View style={{ height: 20 }} />
+      <Button
+        title="Increment"
+        onPress={() => setCount(count + 1)}
+      />
+      <Text>{count}</Text>
       <StatusBar style="auto" />
     </View>
   );
