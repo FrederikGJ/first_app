cf26659de797bf2a1f7bbc73a04b5fe649b08bff
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-07-02 22:55:30 +0200

    updates and so on

--- App.js
+++ App.js
@@ -2,30 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import React, { useState } from 'react';
 
-export default function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <View style={styles.container}>
-      <Text>Dette er en klog og dygtig finnsk laphund. Alle elsker den.</Text>
-      <View style={{ height: 20 }} />
-      <Image
-        style={styles.logo}
-        source={count < 5 ? require('./assets/lapp-small.png') : require('./assets/shiba.jpg')}
-      />
-      <View style={{ height: 20 }} />
-      <Text>Klik på denne knap fem gange og se hvad der sker.</Text>
-      <View style={{ height: 20 }} />
-      <Button
-        title="Increment"
-        onPress={() => setCount(count + 1)}
-      />
-      <Text>{count}</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -38,3 +14,51 @@ const styles = StyleSheet.create({
     height: 200,
   },
 });
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  // Choose the source of the image based on the count
+  let imageSource;
+  if (count < 10) {
+    imageSource = require('./assets/lapp-small.png');
+  } else if (count < 20) {
+    imageSource = require('./assets/shiba.jpg');
+  } else if (count < 30) {
+    imageSource = require('./assets/corgi.png');
+  } else if (count < 40) {
+    imageSource = require('./assets/labradoodle.png')
+  } else if (count < 50) {
+    imageSource = require('./assets/labrador.jpg')
+  } else if (count < 60) {
+    imageSource = require('./assets/golden-retriever.png')
+  } else if (count < 70) {
+    imageSource = require('./assets/dalmatiner.png')
+  } else if (count < 80) {
+    imageSource = require('./assets/beagle.png')
+  } else if (count < 90) {
+    imageSource = require('./assets/australian-sherperd.png')
+  } else {
+    imageSource = require('./assets/yorkshire-terrier.jpg')
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>I denne app kan du se alle hudnene</Text>
+      <View style={{ height: 20 }} />
+      <Image
+        style={styles.logo}
+        source={imageSource}
+      />
+      <View style={{ height: 20 }} />
+      <Text>Klik på denne knap ti gange og se hvad der sker.</Text>
+      <View style={{ height: 20 }} />
+      <Button
+        title="Increment"
+        onPress={() => setCount(count + 1)}
+      />
+      <Text>{count}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
